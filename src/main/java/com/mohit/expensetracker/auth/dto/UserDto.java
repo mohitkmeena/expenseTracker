7310c1b87f150ec968a00118cb4559d86a8a5df2
mohitkmeena <mohitmeenaG2005@gmail.com>
2025-03-01 22:47:55 +0530

    kafka problem fixes

--- src/main/java/com/mohit/expensetracker/auth/dto/UserDto.java
+++ src/main/java/com/mohit/expensetracker/auth/dto/UserDto.java
@@ -1,18 +1,31 @@
 package com.mohit.expensetracker.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.mohit.expensetracker.auth.entity.User;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 
-@Data
+
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Getter
 @Setter
-
+@Data
+@Builder
 public class UserDto extends User {
-    private String username;
-    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
     private String email;
 }
--- src/main/java/com/mohit/expensetracker/auth/service/UserDetailsImpl.java
+++ src/main/java/com/mohit/expensetracker/auth/service/UserDetailsImpl.java
@@ -1,8 +1,8 @@
 package com.mohit.expensetracker.auth.service;
 
 import java.util.HashSet;
-import java.util.Objects;
 
+import com.mohit.expensetracker.auth.eventProducer.UserEventInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,13 +13,14 @@ import org.springframework.stereotype.Service;
 
 import com.mohit.expensetracker.auth.dto.UserDto;
 import com.mohit.expensetracker.auth.entity.User;
+import com.mohit.expensetracker.auth.eventProducer.UserProducer;
 import com.mohit.expensetracker.auth.repository.UserRepository;
 import com.mohit.expensetracker.exceptions.UserNotFoundException;
 @Service
 public class UserDetailsImpl implements UserDetailsService  {
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired private UserProducer userProducer;
     @Autowired
     private ApplicationContext context;
 
@@ -44,9 +45,18 @@ public class UserDetailsImpl implements UserDetailsService  {
          .email(userDto.getEmail())
          .roles(new HashSet<>())
          .build();
-         userRepository.save(user);
+        User user2 = userRepository.save(user);
 
+      userProducer.sendUsertoSave(userEventInfo(userDto,user2.getUserid()));
 
          return true;
+    }
+    private  UserEventInfo  userEventInfo(UserDto user,String userid){
+        return  UserEventInfo.builder().userId(userid)
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
     }
 }
