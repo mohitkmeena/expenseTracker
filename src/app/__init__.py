12c6b34b5aa25fc6b3f8f3aec7670fba05327393
mohitkmeena <mohitmeenaG2005@gmail.com>
2025-03-06 21:27:43 +0530

    data extraction service mmodified , kafka configured

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,26 +1,26 @@
 from flask import Flask
 from service.messageService import MessageService
 from service.expense import Expense
+from kafka import KafkaProducer
+import json
 from flask import request,jsonify
 app =Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messages =MessageService()
+producer =KafkaProducer(bootstrap_servers=['127.0.0.1:9092'],value_serializer=lambda v:json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/ds/message',methods =['POST'])
 def handle_message():
     message=request.json.get('message')
     result=messages.process_message(message=message)
-    print(result)
-    if isinstance (result,Expense):
-       return jsonify(result.to_dict())
-    return jsonify({"response": result}) 
+    s_result=result.json()
+    producer.send("expense",s_result)
 
-@app.route('/v1/ds/get',methods =['GET'])
-def handle_get():
-    print('hello world')
+    return jsonify(result)
+
 
 
 
 if __name__ =="__main__":
-    app.run(host="localhost",port=8004,debug=True)
+    app.run(host="localhost",port=8000,debug=True)
