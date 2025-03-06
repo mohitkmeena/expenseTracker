2d859e7ae776492c8068ed661192a1a7fe28b24a
mohitkmeena <mohitmeenaG2005@gmail.com>
2025-03-07 00:49:59 +0530

    kafka config update

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,24 +3,24 @@ from service.messageService import MessageService
 from service.expense import Expense
 from kafka import KafkaProducer
 import json
+import os
 from flask import request,jsonify
 app =Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messages =MessageService()
-producer =KafkaProducer(bootstrap_servers=['127.0.0.1:9092'],value_serializer=lambda v:json.dumps(v).encode('utf-8'))
+kafka_host=os.getenv('KAFKA_HOST','localhost')
+kafka_port=os.getenv('KAFKA_PORT','9092')
+kafka_server=f'{kafka_host}:{kafka_port}'
+producer =KafkaProducer(bootstrap_servers=[kafka_server],value_serializer=lambda v:json.dumps(v).encode('utf-8'))
 
-@app.route('/v1/ds/message',methods =['POST'])
+@app.route('/api/v1/data-extract',methods =['POST'])
 def handle_message():
     message=request.json.get('message')
     result=messages.process_message(message=message)
     s_result=result.json()
     producer.send("expense",s_result)
-
     return jsonify(result)
-
-
-
 
 if __name__ =="__main__":
     app.run(host="localhost",port=8000,debug=True)
